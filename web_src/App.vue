a744fbd119a36f54436559708272e2091d6fabc8
MMing <1379252510@qq.com>
2024-07-26 02:26:08 +0000

    fix:修复web项目无法编译的问题

---
+++ web_src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+      <transition>
+        <router-view></router-view>
+      </transition>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style>
+  /* 全局样式 */
+  </style>
+  
